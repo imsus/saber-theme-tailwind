e45951e4b0205e0649751c2267bc00040bdb6a5c
Imam Susanto <abc.imams@gmail.com>
2019-03-28 08:44:50 +0700

    Initial Commit

---
+++ layouts/page.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2 class="page-title">{{ page.attributes.title }}</h2>
+    <div class="page-content">
+      <slot name="default"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["page"]
+};
+</script>
---
+++ layouts/post.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2 class="page-title">{{ page.attributes.title }}</h2>
+    <div class="page-content">
+      <slot name="default"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["page"]
+};
+</script>
+
+<style src="../assets/css/main.css"></style>
